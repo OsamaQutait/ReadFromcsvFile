e5e80368e21fa64090d16253f0d4d4dc932e8230
osama <oaqutait@gmail.com>
2022-07-17 12:31:49 +0300

    osama

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ if __name__ == '__main__':
     emp = []
     table = PrettyTable()
     with open("content.csv", 'r') as file:
-        csvreader = csv.reader(file)
-        header = next(csvreader)
+        csvReader = csv.reader(file)
+        header = next(csvReader)
         table.field_names = header
-        for row in csvreader:
+        for row in csvReader:
             emp.append(Employee(int(row[0]), row[1], int(row[2]), row[3], row[4], row[5], int(row[6])))
             table.add_row(row)
     print(table)
